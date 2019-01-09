0c4dcf74cf701bde3a43c2cd06f7597017c69990
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2019-01-09 21:01:18 +0100

    added header and carousel for maadwalk games

---
+++ src/Maadsite.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app dark>
+    <maadheader msg="This is where the header should be"/>
+    <maadcarousel msg="There should be a carousel here if I'm not mistaken..."/>
+
+  </v-app>
+</template>
+
+<script>
+import maadheader from './components/maadheader'
+import maadcarousel from './components/maadcarousel'
+
+export default {
+  name: 'App',
+  components: {
+    maadheader,
+    maadcarousel
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style>
+html { overflow-y: auto }
+</style>
---
+++ src/components/maadheader.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Maadwalk</span>
+        <span class="font-weight-light">Games</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div>
+        <v-btn flat large>Mercenary TD</v-btn>
+        <v-btn flat large>Legionnaires TD</v-btn>
+        <v-btn flat large>News</v-btn>
+        <v-btn flat large>Team</v-btn>
+        <v-btn flat large>About</v-btn>
+      </div>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'maadheader',
+    props: {
+      msg: String
+    }
+}
+</script>
+
+<style>
+
+</style>
