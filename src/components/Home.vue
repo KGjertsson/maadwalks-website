1ea366c4825dde3f571c67547c14c5fda4179487
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2019-01-18 20:44:00 +0100

    update home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,110 @@
+
 <template>
-  <v-container-fluid>
-    <v-carousel hide-delimiters hide-controls height=937>
-      <v-carousel-item
-        v-for="(item,i) in items"
-        :key="i"
-        :src="item.src"
-        reverse-transition="fade"
-        transition="fade"
-      ></v-carousel-item>
-    </v-carousel>
-    <h1>Feed from google play store</h1>
-  </v-container-fluid>
+  <!-- http://play.google.com/store/apps/details?id=com.google.android.apps.maps -->
+  <v-container
+    fluid
+    style="
+    padding-left: 0px;
+    padding-bottom: 0px;
+    padding-right: 0px;
+    padding-top: 0px;
+"
+  >
+    <v-layout align-end justify-center row>
+      <v-img src="https://i.imgur.com/EdAGGFS.jpg" style="height: 100vh">
+        <v-btn large>Play now</v-btn>
+      </v-img>
+    </v-layout>
+
+    <v-layout justify-center>
+      <v-flex xs6 sm12>
+        <v-card flat style="background-color: transparent">
+          <v-container fluid grid-list-md>
+            <v-layout row center>
+              <v-flex>
+                <v-card style="margin: 10px">
+                  <v-container fill-height fluid pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text">Hallå eller!</span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+
+                  <v-img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Kjell_tv.jpg/200px-Kjell_tv.jpg"
+                    height="400px"
+                  ></v-img>
+                  <v-card-actions>
+                    <v-container fill-height fluid pa-2>
+                      <v-layout fill-height>
+                        <v-flex xs12 align-end flexbox>
+                          <span class="headline white--text">Hallå eller!</span>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                      <v-icon>email</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+      <v-flex xs6 sm12>
+        <v-card flat style="background-color: transparent">
+          <v-container fluid grid-list-md>
+            <v-layout row center>
+              <v-flex>
+                <v-card style="margin: 10px">
+                  <v-container fill-height fluid pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text">Hallå eller!</span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+
+                  <v-img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Kjell_tv.jpg/200px-Kjell_tv.jpg"
+                    height="400px"
+                  ></v-img>
+                  <v-card-actions>
+                    <v-container fill-height fluid pa-2>
+                      <v-layout fill-height>
+                        <v-flex xs12 align-end flexbox>
+                          <span class="headline white--text">Hallå eller!</span>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                      <v-icon>email</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        items: [
-          {
-            src: 'https://i.imgur.com/EdAGGFS.jpg'
-          },
-          {
-            src: 'https://pixelz.cc/wp-content/uploads/2017/12/mercedes-benz-amg-gt-r-uhd-4k-wallpaper.jpg'
-          },
-          {
-            src: 'https://images8.alphacoders.com/712/712496.jpg'
-          },
-          {
-            src: 'https://gameranx.com/wp-content/uploads/2017/03/Prey-4K-Wallpaper.jpg'
-          }
-        ]
-      }
-    }
+export default {
+  data() {
+    return {};
   }
+};
 </script>
+
+<style>
+.container.fluid {
+  padding: 0px;
+}
+</style>
