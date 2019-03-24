755960fe0e20d3bb0bba813b098ec8f61a09505f
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2019-03-24 20:12:09 +0100

    added background for team, updated code for team, started reworking contact where I changed form to email and reddit links

--- src/components/MaadContact.vue
+++ src/components/MaadContact.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container fluid grid-list-md>
-    <v-layout justify-center align-center>
-      <h1>Contact development team:</h1>
+  <v-container>
+    <v-layout align-center justify-center row fill-height>
+      <v-flex sm8 fluid>
+        <v-card>
+          <v-card-title>Contact Maadwalk Games:</v-card-title>
+          <v-card-text>Email us at maadwalk@gmail.com</v-card-text>
+        </v-card>
+      </v-flex>
     </v-layout>
-
-    <v-card>
-      <v-textarea name="input-7-1" box label="What's on your mind?" auto-grow></v-textarea>
-    </v-card>
-    <v-btn block color="primary" dark>Send Email</v-btn>
   </v-container>
 </template>
--- src/components/MaadTeam.vue
+++ src/components/MaadTeam.vue
@@ -1,87 +1,137 @@
 <template>
-  <v-container style="padding: 0px" fluid grid-list-md text-xs-center>
-    <v-layout align-end justify-center row fill-height>
-      <v-flex xs8 align-end flexbox>
-        <h1>Maadwalk Games</h1>
-        <span>The foundation of what would later become the Alliance Dota 2 team was laid when Swedish DotA veteran Loda, who had just left Singaporean Team Zenith after their dissolution following The International 2012, was approached by a sponsor (which would later become the Alliance organization) asking him if he was interested in putting together a team.[1] Looking back at the glory days of his early career, his decision was to try to go with an all-swedish lineup in the same spirit as his oldest and most successful team, SK Gaming of the 2008 era. He and fellow Swedish support veteran Akke's, eyes fell upon rising Swedish solo-mid star s4, whom Loda had noticed during their HoN days. s4 and AdmiralBulldog were playing for the new upstart team No Tidehunter created by s4 and Canadian support player EternaLEnVy. In late October 2012, it was announced that Loda and Akke had joined team No Tidehunter,[2] only one month before Dreamhack Winter 2012 which the team had qualified for with a mixed roster including German carry Black^.</span>
-      </v-flex>
-    </v-layout>
+  <v-layout fill-height>
+    <v-flex xs12 sm12>
+      <v-card>
+        <v-img class="white--text" :src="background">
+          <v-container style="padding: 0px" text-xs-center>
+            <v-flex xs12 sm8 offset-sm2>
+              <h1>Maadwalk Games</h1>
+              <span>
+                Maadwalk Games consists of a small and dedicated team brought together by a shared love for Warcraft III and
+                Legion TD. Our flagship product, Mercenary TD, is a remake of the original WC3 game for android, which maintains
+                the true spirit of Legion TD yet filled with exciting new content.
+              </span>
+            </v-flex>
 
-    <v-layout justify-center row wrap>
-      <v-flex v-for="card in cardsRowOne" :key="card.title" shrink style="padding: 20px">
-        <v-card>
-          <v-container fill-height pa-2>
-            <v-layout fill-height>
-              <v-flex xs12 align-end flexbox>
-                <span class="headline white--text" v-text="card.name"></span>
+            <v-layout justify-center row wrap>
+              <v-flex v-for="card in cardsRowOne" :key="card.title" shrink style="padding: 20px">
+                <v-card>
+                  <v-container fill-height pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.name"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+
+                  <v-img :src="card.src" height="200px"></v-img>
+                  <v-card-actions>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                      <v-icon>email</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+
+            <v-layout justify-center row wrap>
+              <v-flex v-for="card in cardsRowTwo" :key="card.title" shrink style="padding: 20px">
+                <v-card>
+                  <v-container fill-height pa-2>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <span class="headline white--text" v-text="card.name"></span>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+
+                  <v-img :src="card.src" height="200px"></v-img>
+                  <v-card-actions>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                    <v-spacer></v-spacer>
+                    <v-btn icon onclick="window.open('mailto:konrad.gjertsson@protonmail.com')">
+                      <v-icon>email</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
               </v-flex>
             </v-layout>
           </v-container>
+        </v-img>
+      </v-card>
+    </v-flex>
+  </v-layout>
 
-          <v-img :src="card.src" height="200px"></v-img>
-          <v-card-actions>
-            <v-flex xs12 align-end flexbox>
-              <span class="headline white--text" v-text="card.title"></span>
-            </v-flex>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>email</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
+  <!-- <v-img :src="background" flexbox>
+    <v-container style="padding: 0px" text-xs-center>
+      <v-layout align-end justify-center row fill-height>
+        <v-flex xs8 align-end flexbox>
+          <h1>Maadwalk Games</h1>
+          <span>
+            Maadwalk Games consists of a small and dedicated team brought together by a shared love for Warcraft III and
+            Legion TD. Our flagship product, Mercenary TD, is a remake of the original WC3 game for android, which maintains
+            the true spirit of Legion TD yet filled with exciting new content.
+          </span>
+        </v-flex>
+      </v-layout>
 
-    <v-layout justify-center row wrap>
-      <v-flex v-for="card in cardsRowTwo" :key="card.title" shrink style="padding: 20px">
-        <v-card>
-          <v-container fill-height pa-2>
-            <v-layout fill-height>
+      <v-layout justify-center row wrap>
+        <v-flex v-for="card in cardsRowOne" :key="card.title" shrink style="padding: 20px">
+          <v-card>
+            <v-container fill-height pa-2>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text" v-text="card.name"></span>
+                </v-flex>
+              </v-layout>
+            </v-container>
+
+            <v-img :src="card.src" height="200px"></v-img>
+            <v-card-actions>
               <v-flex xs12 align-end flexbox>
-                <span class="headline white--text" v-text="card.name"></span>
+                <span class="headline white--text" v-text="card.title"></span>
               </v-flex>
-            </v-layout>
-          </v-container>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>email</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
 
-          <v-img :src="card.src" height="200px"></v-img>
-          <v-card-actions>
-            <v-flex xs12 align-end flexbox>
-              <span class="headline white--text" v-text="card.title"></span>
-            </v-flex>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>email</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
+      <v-layout justify-center row wrap>
+        <v-flex v-for="card in cardsRowTwo" :key="card.title" shrink style="padding: 20px">
+          <v-card>
+            <v-container fill-height pa-2>
+              <v-layout fill-height>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text" v-text="card.name"></span>
+                </v-flex>
+              </v-layout>
+            </v-container>
 
-    <v-layout justify-center row wrap>
-      <v-flex v-for="card in cardsRowThree" :key="card.title" shrink style="padding: 20px">
-        <v-card>
-          <v-container fill-height pa-2>
-            <v-layout fill-height>
+            <v-img :src="card.src" height="200px"></v-img>
+            <v-card-actions>
               <v-flex xs12 align-end flexbox>
-                <span class="headline white--text" v-text="card.name"></span>
+                <span class="headline white--text" v-text="card.title"></span>
               </v-flex>
-            </v-layout>
-          </v-container>
-
-          <v-img :src="card.src" height="200px"></v-img>
-          <v-card-actions>
-            <v-flex xs12 align-end flexbox>
-              <span class="headline white--text" v-text="card.title"></span>
-            </v-flex>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>email</v-icon>
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+              <v-spacer></v-spacer>
+              <v-btn icon onclick="window.open('mailto:konrad.gjertsson@protonmail.com')">
+                <v-icon>email</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-img>-->
 </template>
 
 <script>
@@ -115,7 +165,8 @@ export default {
         title: "Web Developer",
         src: require("@/assets/Kjell_tv.jpg")
       }
-    ]
+    ],
+    background: require("@/assets/82239.jpg")
   })
 };
 </script>
