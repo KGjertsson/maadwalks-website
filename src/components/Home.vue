ca8e91a044b79d2b6c7993eda090d06601fb5afd
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2019-04-21 12:42:32 +0200

    added company logo, minor other updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,13 @@
 
 <template>
   <v-container fluid style="padding: 0px">
-    <v-img :src="background" style="height: 100vh"></v-img>
+    <v-img :src="background" style="height: 100vh">
+      <v-layout align-center justify-center row fill-height>
+        <v-flex>
+          <v-img :src="logo" style="height: 600px; width: 600px;"></v-img>
+        </v-flex>
+      </v-layout>
+    </v-img>
 
     <v-layout align-end justify-center row>
       <v-img :src="mercenary_image" style="height: 100vh">
@@ -18,6 +24,7 @@
 export default {
   data: () => ({
     background: require("@/assets/16503_edited.png"),
+    logo: require("@/assets/14329_edited_500.png"),
     mercenary_image: require("@/assets/screen-4.jpg")
   })
 };
--- src/components/MaadDataPolicy.vue
+++ src/components/MaadDataPolicy.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>We don't care nuthin' about no data</h1>
+  <h1>We don't care nuthin' about no data</h1>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    background: require("@/assets/16503_edited.png")
+  })
+};
+</script>
