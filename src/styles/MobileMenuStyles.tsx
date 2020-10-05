241f90a81839bcccf01f19624b0fc4446fc42f87
KGjertsson <gjertsson.konrad@gmail.com>
2020-10-05 08:29:18 +0200

    fixed positioning and links of mobile menu

--- src/styles/MobileMenuStyles.tsx
+++ src/styles/MobileMenuStyles.tsx
@@ -3,7 +3,6 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     menu: {
-      position: 'absolute',
       right: 0,
     },
   })
